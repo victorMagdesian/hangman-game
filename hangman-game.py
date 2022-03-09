521f909f44ae40ef59124b4cd6024ce722082bda
victorMagdesian <vicfemagdesian@gmail.com>
2022-03-08 21:34:20 -0300

    v1

---
+++ hangman-game.py
@@ -0,0 +1,73 @@
+from random import random
+import random
+
+hangStep = 0
+
+words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
+
+answer = random.choice(words)
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========\n''']
+
+while hangStep < 6:
+    print(HANGMANPICS[hangStep])
+    playerInput = input("insira a letra escolhida\n")
+    if playerInput not in answer:
+        hangStep += 1
+        if hangStep == 6:
+            print(HANGMANPICS[hangStep]+"GAME OVER\n"+"WORD WAS "+ answer)
+
